8c75c10dbcb1354fb384ad7b44cdfcb25c730fe6
Marcos <marcos.antas@hotmail.com.br>
2015-02-10 19:36:56 -0300

    Fib2

--- aula1/js.js
+++ aula1/js.js
@@ -1,26 +1,19 @@
 ﻿function fibDOM()
 {
 	var x = parseInt(document.getElementById("textbox").value);
-	var numeros = [];
-	numeros[0] = 0;
-	numeros[1] = 1;
-	for(i = 2; i <= x; i++)
+	if(x <= 0)
 	{
-		numeros[i] = numeros[i-1] + numeros[i-2];
+		document.getElementById("resultado").innerHTML += "<br/> Fib(0) = 0";
 	}
-	document.getElementById("resultado").innerHTML += "<br/> Fib(" + x + ") = " + numeros;
-}
-
-function fibJQuery()
-{
-	alert($("#textbox").val());
-	var x = parseInt($("#textbox").value());
-	var numeros = [];
-	numeros[0] = 0;
-	numeros[1] = 1;
-	for(i = 2; i <= x; i++)
+	else
 	{
-		numeros[i] = numeros[i-1] + numeros[i-2];
+		var numeros = [];
+		numeros[0] = 0;
+		numeros[1] = 1;
+		for(i = 2; i <= x; i++)
+		{
+			numeros[i] = numeros[i-1] + numeros[i-2];
+		}
+		document.getElementById("resultado").innerHTML += "<br/> Fib(" + x + ") = " + numeros;
 	}
-	$("#resultado").html("ok");
 }
