c42951ad1e05580979a8702de1033e3391af6bc3
Marcos <marcos.antas@hotmail.com.br>
2015-02-10 19:33:44 -0300

    Aula 1

---
+++ aula1/js.js
@@ -0,0 +1,26 @@
+﻿function fibDOM()
+{
+	var x = parseInt(document.getElementById("textbox").value);
+	var numeros = [];
+	numeros[0] = 0;
+	numeros[1] = 1;
+	for(i = 2; i <= x; i++)
+	{
+		numeros[i] = numeros[i-1] + numeros[i-2];
+	}
+	document.getElementById("resultado").innerHTML += "<br/> Fib(" + x + ") = " + numeros;
+}
+
+function fibJQuery()
+{
+	alert($("#textbox").val());
+	var x = parseInt($("#textbox").value());
+	var numeros = [];
+	numeros[0] = 0;
+	numeros[1] = 1;
+	for(i = 2; i <= x; i++)
+	{
+		numeros[i] = numeros[i-1] + numeros[i-2];
+	}
+	$("#resultado").html("ok");
+}
